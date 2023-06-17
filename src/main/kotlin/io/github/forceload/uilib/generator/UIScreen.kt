50cbeb662b1c5b076de8a6ed7d13ddf2accbebd1
컨트롤D <controld3785@gmail.com>
2023-06-17 17:48:14 +0900

    Add UIScreen KDoc
    KDoc Update:
    - Add UIScreen Example

--- src/main/kotlin/io/github/forceload/uilib/generator/UIScreen.kt
+++ src/main/kotlin/io/github/forceload/uilib/generator/UIScreen.kt
@@ -8,10 +8,30 @@ import net.minecraft.client.gui.screen.Screen
 import net.minecraft.text.Text
 
 /**
- * This class is one of the most essential parts of using this library.
+ * UIScreen is one of the most essential parts of using this library
  *
  * ## Example
  * ```kotlin
+ * // This is a sample screen with a square button which says "Sample" in the center
+ * class SampleScreen(title: String): Screen(title) {
+ *     override fun init() {
+ *         ui = buildUI { //Build Main UI
+ *             button("Sample") {
+ *                 position = Point2D(width / 2, height / 2)
+ *                 centered = true
+ *             }
+ *         }
+ *     }
+ *
+ *     override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
+ *         this.renderBackground(context)
+ *         context.let { ui.render(it, mouseX, mouseY, delta) } // Render UI Components
+ *
+ *         super.render(context, mouseX, mouseY, delta)
+ *     }
+ *
+ *     override fun tick() { ui.update() } // Update UI (Useless in this example)
+ * }
  * ```
  */
 open class UIScreen(title: Text?) : Screen(title) {
